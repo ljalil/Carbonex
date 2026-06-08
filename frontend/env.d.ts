8adc93524a55a07da84f63c5e5e1c1427b6e87bc
ljalil <abdeljalilletrache@outlook.com>
2026-06-07 20:32:35 -0600

    Restructure

--- frontend/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-// Declare Vue single-file components
-declare module '*.vue' {
-	import type { DefineComponent } from 'vue';
-	const component: DefineComponent<{}, {}, any>;
-	export default component;
-}
--- frontend/src/components/tabs/index.js
+++
@@ -1,4 +0,0 @@
-// Tab components exports
-export { default as SolubilityTrappingTab } from './SolubilityTrappingTab.vue';
-export { default as MineralTrappingTab } from './MineralTrappingTab.vue';
-export { default as SensitivityAnalysisTab } from './SensitivityAnalysisTab.vue';
